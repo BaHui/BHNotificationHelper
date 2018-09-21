b493dc5671e73de89b4ee67027ac68699ff6b4ca
BaHui·Qiao <qiaobahuiyouxiang@163.com>
2018-09-21 12:58:18 +0800

    添加备注

--- BHNotificationHelper/BHNotificationHelper.h
+++ BHNotificationHelper/BHNotificationHelper.h
@@ -19,15 +19,16 @@
 - (void)addNotificationForName:(NSString *)notificationName response:(void (^)(NSDictionary * userInfo))response;
 
 /**
- * 移除某个通知
+ * 仅用于移除单个通知
+ * 提示: 界面释放时, 会自动移除所有的Observer, 无需手动调用.
  * @param notificationName 被移除的通知的名字
  */
 - (void)removeNotificationForName:(NSString *)notificationName;
 
 /**
  * 发送通知
- @param notificationName 通知名称
- @param userInfo 通知传值对象
+ * @param notificationName 通知名称
+ * @param userInfo 通知传值
  */
 - (void)postNotificationForName:(NSString *)notificationName userInfo:(NSDictionary *)userInfo;
 
